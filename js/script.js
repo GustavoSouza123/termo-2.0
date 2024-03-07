a57b876a80af7d3e2b87ef4e04d0845ca485cafd
Gustavo Souza <gustavoelia7@gmail.com>
2024-03-07 09:18:24 -0300

    feats: enable and disable rows, check if current row is complete to go to the next row

--- js/script.js
+++ js/script.js
@@ -6,45 +6,89 @@ const generateGame = () => {
     for(let i = 0; i < 6; i++) {
         game.innerHTML += `<div class="row ${i === 0 ?'active' : 'disabled'}"></div>`;
         for(let j = 0; j < 5; j++) {
-            document.querySelectorAll('.row')[i].innerHTML += `<div class="input" contenteditable="${i === 0}" tabindex="0" index="${j}" onkeydown="${i === 0 ? 'changeInputFocus(event)' : ''}"></div>`;
+            document.querySelectorAll('.row')[i].innerHTML += `<div class="input" contenteditable="${i === 0}" tabindex="0" index="${j}" onkeydown="changeInputFocus(event)" onclick="inputClick(event)"></div>`;
         }
     }
 }
 
+const inputClick = (e) => {
+    currentInput = e.target.getAttribute('index');
+    console.log('focus', currentInput, currentRow)
+}
+
 const changeInputFocus = (e) => {
-    // console.log(e.target)
+    // console.log(e.keyCode)
+    
+    currentInput = +e.target.getAttribute('index') + 1;
 
     if(e.key.match(/^[a-zA-Z]$/g)) {
         if(e.target.innerText != '') {
             e.target.innerText = '';
         }
-        currentInput++;
-        if(currentInput % 5 == 0) {
-            currentRow++;
-            document.querySelectorAll('.row')[currentRow].classList.remove('disabled');
-            document.querySelectorAll('.row')[currentRow].classList.add('active');
-            document.querySelectorAll('.row')[currentRow].addEventListener('keydown', () => { changeInputFocus(event) }); // PAREI AQUI 
+    } else if(e.keyCode === 8) {
+        e.target.innerText = '';
+        if(currentInput > 0) {
             setTimeout(() => {
-                document.querySelectorAll('.row')[currentRow].firstChild.focus();
+                e.target.previousSibling.focus();
             }, 10);
+        }
+    } else if(e.keyCode === 32) {
+        setTimeout(() => {
+            e.target.nextSibling.focus();
+        }, 10);
+    }
+
+    setTimeout(() => {
+        if(currentInput % 5 == 0) {
+            let rowComplete = checkRowComplete();
+            if(rowComplete) {
+                currentRow++;
+                if(currentRow == document.querySelectorAll('.row').length) {
+                    console.log('O jogo acabou');
+                    e.target.blur();
+                    disableRow(currentRow-1);
+                    return;
+                } else {
+                    disableRow(currentRow-1);
+                    enableRow(currentRow);
+                    setTimeout(() => {
+                        document.querySelectorAll('.row')[currentRow].firstChild.focus();
+                    }, 10);
+                }
+            }
         } else {
             setTimeout(() => {
                 e.target.nextSibling.focus();
             }, 10);
         }
-    } else if(e.keyCode === 8) {
-        e.target.innerText = '';
-        currentInput--;
-        if(currentInput < 0) {
-            currentInput = 0;
-        } else {
-            setTimeout(() => {
-                e.target.previousSibling.focus();
-            }, 10);
+    }, 10)
+
+    console.log('focus', currentInput, currentRow)
+}
+
+const disableRow = (id) => {
+    document.querySelectorAll('.row')[id].classList.remove ('active');
+    document.querySelectorAll('.row')[id].classList.add('disabled');
+    document.querySelectorAll('.row')[id].querySelectorAll('.input').forEach((input) => input.setAttribute('tabindex', '-1'));
+    document.querySelectorAll('.row')[id].querySelectorAll('.input').forEach((input) => input.setAttribute('onclick', ''));
+}
+
+const enableRow = (id) => {
+    document.querySelectorAll('.row')[id].classList.remove('disabled');
+    document.querySelectorAll('.row')[id].classList.add('active');
+    document.querySelectorAll('.row')[id].querySelectorAll('.input').forEach((input) => input.setAttribute('contenteditable', 'true'));
+}
+
+const checkRowComplete = () => {
+    let inputs = document.querySelectorAll('.row')[currentRow].querySelectorAll('.input');
+    for(i = 0; i < inputs.length; i++) {
+        if(inputs[i].innerText == '') {
+            console.log(i, false);
+            return false;
         }
     }
-
-    console.log(currentInput, currentRow)
+    return true;
 }
+
 
 generateGame();
